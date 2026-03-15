4f032920026a21ed51581c01f200e6b4ec78bec4
dennerricardo <denner.ricardo@gmail.com>
2026-03-15 19:50:39 -0300

    partial graphs algorithms

---
+++ src/main/java/dev/graphs/DepthFirstSearch.java
@@ -0,0 +1,77 @@
+package dev.graphs;
+
+import java.util.*;
+
+public class DepthFirstSearch {
+
+    static Map<String, List<String>> graph = new LinkedHashMap<>();
+
+    public static void addEdge(String from, String to ){
+        graph.computeIfAbsent(from, k -> new ArrayList<>()).add(to);
+        graph.computeIfAbsent(to, k -> new ArrayList<>()).add(from);
+    }
+
+    public static List<String> DepthFirstSearch(String Start){
+        List<String> visitOrder  = new ArrayList<>();
+        Set<String> visited = new HashSet<>();
+        Queue<String> queue = new LinkedList<>();
+
+        visited.add(start);
+        queue.offer(start);
+
+        System.out.println("=== Depth First Search starting from: " + start + " ===");
+
+        while (!queue.isEmpty()){
+            String current = queue.poll();
+            visitOrder.add(current);
+            System.out.println("Visiting : " + current);
+
+            for (String neighbor : graph.getOrDefault(current, new ArrayList<>())){
+                if(!visited.contains(neighbor)){
+                    visited.add(neighbor);
+                    queue.offer(neighbor);
+                    System.out.println(" -> Enqueued: " + neighbor);
+                }
+            }
+        }
+
+        return visitOrder;
+    }
+
+    public static List<String> shortestPath(String start, String end){
+        if(start.equals(end)) return List.of(start);
+        Map<String, String> parentMap = new HashMap<>();
+        Set<String> visited = new HashSet<>();
+        Queue<String> queue = new LinkedList<>();
+
+        visited.add(start);
+        queue.offer(start);
+        parentMap.put(start, null);
+
+        while (!queue.isEmpty()){
+            String current = queue.poll();
+
+            for (String neighbor : graph.getOrDefault(current, new ArrayList<>())){
+                if(!visited.contains(neighbor)){
+                    visited.add(neighbor);
+                    parentMap.put(neighbor,current);
+                    queue.offer(neighbor);
+
+                    if(neighbor.equals(end)){
+
+                        List<String> path = new ArrayList<>();
+                        String node = end;
+                        while (node != null){
+                            path.add(0, node);
+                            node = parentMap.get(node);
+                        }
+                        return path ;
+                    }
+                }
+            }
+        }
+
+        return new ArrayList<>();
+    }
+
+}
